d5c73a41ae7c505a0ea20a5eeeeea3e969a539d3
Your Name <you@example.com>
2021-08-25 10:19:16 +0100

    added helloworld exercise answer

---
+++ src/main/java/HelloWorld/helloWorld.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class helloWorld {
+	String str = "HelloWorld!";
+	public void printMessage() {
+		System.out.println(str);
+	}
+	public void printMsg(String message) {
+		System.out.println(message);
+	}
+}
---
+++ src/main/java/HelloWorld/runner.java
@@ -0,0 +1,23 @@
+package HelloWorld;
+
+public class runner {
+	public static void main(String[] args) {
+		helloWorld h = new helloWorld();
+
+// Part 1
+	System.out.println("HelloWorld!");
+		
+// Part 2
+	final String s = "HelloWorld!"; 
+	System.out.println(s);	
+
+// Part 3
+	
+	h.printMessage();
+	
+// Part 4
+	
+	h.printMsg("HelloWorld!");
+	}
+}
+
